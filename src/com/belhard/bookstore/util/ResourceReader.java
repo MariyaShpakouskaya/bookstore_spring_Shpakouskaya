0b7c0dd03e7137c5c8853a355a4e3eb810303acf
Shpakouskaya_Mariya <Shpackovskaya.maria@yandex.by>
2022-05-04 20:37:41 +0300

    Module 2 (Add Elephant SQl)

--- src/com/belhard/bookstore/util/ResourceReader.java
+++ src/com/belhard/bookstore/util/ResourceReader.java
@@ -8,12 +8,13 @@ import java.util.Properties;
 public class ResourceReader {
 
     private static final String LOCAL_SQL = "src/com/belhard/bookstore/properties/db.properties";
+    private static final String ELEPHANT_SQL = "src/com/belhard/bookstore/properties/elephantsqlDb.properties";
     private static Properties properties;
 
     public ResourceReader() {
         BufferedReader reader;
         try {
-            reader = new BufferedReader(new FileReader(LOCAL_SQL));
+            reader = new BufferedReader(new FileReader(ELEPHANT_SQL));
             properties = new Properties();
             properties.load(reader);
         } catch (IOException e) {
