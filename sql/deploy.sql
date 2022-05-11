bc7e53240b5409f8e28bde5bd42279a6d18394a3
Shpakouskaya_Mariya <Shpackovskaya.maria@yandex.by>
2022-05-11 19:58:24 +0300

    SQL

--- sql/deploy.sql
+++ sql/deploy.sql
@@ -1,27 +1,19 @@
 /*
-DROP TABLE IF EXISTS books;
-DROP TABLE IF EXISTS covers;
+TRUNCATE books CASCADE;
+TRUNCATE covers CASCADE;
+TRUNCATE users CASCADE;
+TRUNCATE roles CASCADE;
 */
-
-CREATE TABLE IF NOT EXISTS covers (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(50)
-);
 
 INSERT INTO covers (name)
 VALUES ('SOFT'),
 ('HARD'),
 ('SPECIAL');
 
-CREATE TABLE IF NOT EXISTS books (
-    id BIGSERIAL PRIMARY KEY,
-	isbn VARCHAR(50) UNIQUE NOT NULL,
-	author VARCHAR(100) NOT NULL,
-	title VARCHAR(100) NOT NULL,
-	price DECIMAL(8,2) DEFAULT 0.0 NOT NULL,
-	cover_id BIGINT REFERENCES covers,
-	deleted BOOLEAN DEFAULT false NOT NULL
-);
+INSERT INTO roles (name)
+VALUES ('ADMIN'),
+('MANAGER'),
+('CUSTOMER');
 
 INSERT INTO books (isbn, author, title, cover_id, price )
 VALUES
@@ -46,7 +38,26 @@ VALUES
 ('978-5-519-02604-8', 'J.London', 'Martin Eden', (SELECT id FROM covers WHERE name = 'SPECIAL'), '45.18'),
 ('9780500517772', 'A.Johnson', 'Improbable libraries', (SELECT id FROM covers WHERE name = 'HARD'), '34.20');
 
-/*
-TRUNCATE books CASCADE;
-TRUNCATE covers CASCADE;
-*/
+INSERT INTO users (first_name, last_name, email, password, role_id)
+VALUES
+('Maryia', 'Fedorova', 'cooking@mail.ru', 'Qwerty1', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Tatiana', 'Samsonova', 'lady_fairy@gmail.com', 'RTYhuj15', (SELECT id FROM roles WHERE name = 'MANAGER')),
+('Nikita', 'Kolosov', 'NikitaKol85@mail.ru', 'Dpkghj85G', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Matvey', 'Irigin', 'Five_tools@mail.ru', 'fhTujh52', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Iryna', 'Magey', 'lesty@yandex.by', 'Rtyhg56', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Nicolay', 'Slizh', 'under_Dog@yandex.by', 'Evgen589', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Maya', 'Petrova', 'hate.evething@mail.ru', 'Ytrhgfnbv123', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Efrem', 'Nosov', 'party1997@gmail.com', 'Tirujg159', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Vladislav', 'Kukushkin', 'krukol@list.ru', 'Tdnjndjfb89', (SELECT id FROM roles WHERE name = 'ADMIN')),
+('Timofey', 'Gorelov', 'fly895@mail.ru', '85fhfjfk', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Sasha', 'Nikitina', 'gerryOpen@yahoo.com', 'Asdfgh15', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Tonya', 'Rey', 'filePloy@mail.ru', 'Astel96', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Sofiya', 'Avgustyna', 'kimberly@mail.ru', 'Hytgbfn55hh', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Vladislav', 'Lebedev', 'gladeTwo@gmail.com', 'Yqhhhhs85', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Dzenis', 'Dred', 'familyDrive88@mail.ru', 'pepepe7878', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Olya', 'Sivtsova', 'coolll999@gmail.com', 'Qwerty5663', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Sergey', 'Opelin', 'dobby555@list.ru', 'wwuutt', (SELECT id FROM roles WHERE name = 'MANAGER')),
+('Liza', 'Breyd', 'try_again@yahoo.com', '123456', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Petr', 'Budnic', 'Ti_12_52@mail.ru', '5674fhfhf', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Yuliya', 'Konovalova', 'Pabloking@mail.ru', '85Hey96', (SELECT id FROM roles WHERE name = 'CUSTOMER')),
+('Nikita', 'Shevtsov', 'Nikita_Shevtsov_78@yahoo.com', 'ErtyfhgY5', (SELECT id FROM roles WHERE name = 'CUSTOMER'));
