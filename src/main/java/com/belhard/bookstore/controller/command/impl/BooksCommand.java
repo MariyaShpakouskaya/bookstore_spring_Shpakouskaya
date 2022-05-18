c1ffeb67519fdde0f31ff76cddc899acf850ecad
Shpakouskaya_Mariya <Shpackovskaya.maria@yandex.by>
2022-05-19 00:50:09 +0300

    Module 1 (@Controller)

--- src/main/java/com/belhard/bookstore/controller/command/impl/BooksCommand.java
+++ src/main/java/com/belhard/bookstore/controller/command/impl/BooksCommand.java
@@ -2,18 +2,29 @@ package com.belhard.bookstore.controller.command.impl;
 
 import com.belhard.bookstore.controller.command.Command;
 import com.belhard.bookstore.service.BookService;
-import com.belhard.bookstore.service.BookServiceImpl;
 import com.belhard.bookstore.service.dto.BookDto;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+@Controller("books")
 public class BooksCommand implements Command {
-    private static final BookService BOOK_SERVICE = new BookServiceImpl();
+    private BookService bookService;
+
+    public BooksCommand(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @Autowired
+    public void setBookService(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     public String execute(HttpServletRequest request) {
         Boolean withNo = Boolean.valueOf(request.getParameter("no"));
-        List<BookDto> bookDtos = BOOK_SERVICE.getAllBooks();
+        List<BookDto> bookDtos = bookService.getAllBooks();
         request.setAttribute("books", bookDtos);
         request.setAttribute("no", withNo);
         return "jsp/books.jsp";
--- src/main/java/com/belhard/bookstore/controller/command/impl/UsersCommand.java
+++ src/main/java/com/belhard/bookstore/controller/command/impl/UsersCommand.java
@@ -2,18 +2,29 @@ package com.belhard.bookstore.controller.command.impl;
 
 import com.belhard.bookstore.controller.command.Command;
 import com.belhard.bookstore.service.UserService;
-import com.belhard.bookstore.service.UserServiceImpl;
 import com.belhard.bookstore.service.dto.UserDto;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+@Controller("users")
 public class UsersCommand implements Command {
-    private static final UserService USER_SERVICE = new UserServiceImpl();
+    private UserService userService;
+
+    public UsersCommand(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
     public String execute(HttpServletRequest request) {
         Boolean withNo = Boolean.valueOf(request.getParameter("no"));
-        List<UserDto> userDtos = USER_SERVICE.getAllUsers();
+        List<UserDto> userDtos = userService.getAllUsers();
         request.setAttribute("users", userDtos);
         request.setAttribute("no", withNo);
         return "jsp/users.jsp";
